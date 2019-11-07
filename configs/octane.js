c056691985fe897ce15668ce55191d0211049f69
Jan Buschtöns <jan.buschtoens@clark.de>
2019-11-08 00:43:11 +0100

    docs(octane): add doc blocks

--- configs/octane.js
+++ configs/octane.js
@@ -1,9 +1,16 @@
 module.exports = {
   extends: 'octane',
   rules: {
+    /**
+     * @see https://github.com/ember-template-lint/ember-template-lint/blob/master/docs/rule/no-curly-component-invocation.md
+     */
     'no-curly-component-invocation': {
       allow: require('../known-helpers')
     },
+
+    /**
+     * @see https://github.com/ember-template-lint/ember-template-lint/blob/master/docs/rule/simple-unless.md
+     */
     'simple-unless': {
       whitelist: [],
       maxHelpers: 1
