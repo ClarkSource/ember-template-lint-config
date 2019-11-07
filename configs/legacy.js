64f059d01c31e71f008e0577eeee53e7666cc09d
Jan Buschtöns <jan.buschtoens@clark.de>
2019-11-08 00:42:53 +0100

    fix(legacy): remove `no-positive-tabindex`

--- configs/legacy.js
+++ configs/legacy.js
@@ -1,9 +1,19 @@
 module.exports = {
   extends: 'clark:octane',
   rules: {
+    /**
+     * @see https://github.com/ember-template-lint/ember-template-lint/blob/master/docs/rule/no-implicit-this.md
+     */
     'no-implicit-this': false,
+
+    /**
+     * @see https://github.com/ember-template-lint/ember-template-lint/blob/master/docs/rule/no-curly-component-invocation.md
+     */
     'no-curly-component-invocation': false,
-    'no-positive-tabindex': false,
+
+    /**
+     * @see https://github.com/ember-template-lint/ember-template-lint/blob/master/docs/rule/no-action.md
+     */
     'no-action': false
   }
 };
